8f8a133d68030cd155b38f16e17e330a3eac72e0
Maggiexuiscute <your.maggiexuiscute@gmail.com>
2024-02-21 21:42:22 -0600

    Stop tracking key.js

--- key.js
+++
@@ -1,3 +0,0 @@
-const apiKey = 'AIzaSyD_dW0t6zZJCzQ8OI2LFqy4W7gWmrJ33oI';
-const spreadsheetId = '1ktfafXhz0JqbKn112hgQcHnbFJzIfsff4gDHHXaLJ10';
-const range = 'Sheet1!A2:E14'; // Adjust the range according to your needs
